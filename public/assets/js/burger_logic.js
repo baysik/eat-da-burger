ce506a8fb3505806087d94d37e4faa4e91716af6
baysik <iambaysik@gmail.com>
2019-08-19 09:46:53 -0600

    devour button now functional

--- public/assets/js/burger_logic.js
+++ public/assets/js/burger_logic.js
@@ -1,27 +1,43 @@
-// on click, it should set devoured to true and not show it in the list
-$(".devour-the-burger").on("click", function(event) {
-     let id = $(this).data("burgerid");
-     console.log(id);
-     alert("clicked");
+$(function() {
+     $(".devour-the-burger").on("click", function(event) {
+          let id = $(this).data("burgerid");
+          let id2 = $(this).data("burgerstate");
+          console.log(id, id2);
 
-     // var devouredState = {
-     //      devoured: true
-     // };
-     // // Send the PUT request.
+          var devouredState = {
+               devoured: 1
+          };
+          // Send the PUT request.
 
-     // $.ajax("/api/burgers/" + id, {
-     //      type: "PUT",
-     //      data: devouredState
-     // }).then(function() {
-     //      console.log("changed sleep to", devouredState);
-     //      // Reload the page to get the updated list
-     //      location.reload();
-     // });
-});
+          $.ajax("/api/burgers/" + id, {
+               type: "PUT",
+               data: devouredState
+          }).then(function() {
+               console.log("changed sleep to", devouredState);
+               // Reload the page to get the updated list
+               location.reload();
+          });
+     });
 
-// on click, it should add a burger to the list to devour.
-$("#submit").on("click", function(event) {
-     event.preventDefault();
-     console.log("lol, wow");
-     alert("hello");
+     // on click, it should add a burger to the list to devour.
+     $(".add-burger").on("submit", function(event) {
+          event.preventDefault();
+          console.log($("[name=burger-name]").val());
+
+          var newBurger = {
+               burger_name: $("#burger-to-order").val(),
+               devoured: 0
+          };
+          // Send the PUT request.
+          console.log(newBurger);
+
+          $.ajax("/api/burgers", {
+               type: "POST",
+               data: newBurger
+          }).then(function() {
+               console.log("created new burger");
+               // Reload the page to get the updated list
+               //location.reload();
+          });
+     });
 });
